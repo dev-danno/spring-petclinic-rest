b24a73f9c9acb502abc37f64e2e717a3a381508d
Julissa Castillo <julissa_2892@hotmail.com>
2025-08-22 18:00:00 -0500

    build: added second comment in main

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -10,5 +10,6 @@ public class PetClinicApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(PetClinicApplication.class, args);
         // Added random comment here - 1
+        // Added random comment here - 2
 	}
 }
